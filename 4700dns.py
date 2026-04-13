49460eb9850d2abcd99f4124a8abcf4868759874
Rilayyy <rileymil05@yahoo.com>
2026-04-13 18:43:36 -0400

    resolved my conflicts

--- 4700dns.py
+++ 4700dns.py
@@ -3,18 +3,19 @@
 import argparse, socket, time, json, select, struct, sys, math
 from dnslib import DNSRecord, DNSHeader, RR, QTYPE, A
 from cache import DNSCache
+from collections import defaultdict
 
 class Server:
     def __init__(self, root_ip, domain, port):
         self.root_ip = root_ip
-        self.domain = domain
+        self.domain = ""
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(("0.0.0.0", port))
         self.port = self.socket.getsockname()[1]
 
-        self._parse_zone_file(args.zone)
-        self.cache = DNSCache()
+        self.authoritative_records = defaultdict(list)
+        self._parse_zone_file(domain)
         self.log("Bound to port %d" % self.port)
 
     def _parse_zone_file(self, zone_file):
@@ -54,12 +55,45 @@ class Server:
         request = DNSRecord.parse(data)
         self.log("Received message:\n%s" % request)
 
+        qname = str(request.q.qname)
+        qtype = request.q.qtype
+
         # YOU WILL NEED TO ACTUALLY DO SOMETHING SMART HERE
         # WE ARE JUST REPLYING WITH A FIXED RESPONSE
         response = request.reply()
-        response.add_answer(RR("abc.com", QTYPE.A, rdata=A("1.2.3.4"), ttl=60))       
+
+        if qname.endswith(self.domain):
+            response.header.aa = 1
+
+            correct_records = self.authoritative_records.get((qname, qtype), [])
+
+            if correct_records: 
+                for record in correct_records:
+                    response.add_answer(record)
+
+            else:
+                response.header.rcode = 3
+            
+            self.send(addr, response)
         
-        self.send(addr, response)
+        else:
+            external_response = self._resolve_external(request)
+        
+            if external_response:
+                self.send(addr, external_response)
+            else:
+                response.header.rcode = 3
+                self.send(addr, response)
+        
+
+    def _resolve_external(self, request):
+        """
+        Resolves the request to an external server
+
+        :param request: The request to resolve
+        :return: The response
+        """
+    
 
     def run(self):
         seq = 0
